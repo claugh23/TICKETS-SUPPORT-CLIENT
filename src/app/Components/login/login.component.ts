ffcaec4f0a475973dd3b07ad97cc9d1b33a9fcb2
Claudio Gonzalez Hernandez <40004826+Claudio-2019@users.noreply.github.com>
2021-04-23 08:34:51 -0600

    cambio en package json

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,15 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-=======
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
->>>>>>> a6219970dab3fc6ab5a713d0084503d244244c6d
 import { Router } from '@angular/router';
 import { AuthModel } from 'src/app/Interfaces/IAuth';
 import { UsersModel } from 'src/app/Interfaces/IUsers';
@@ -37,11 +28,7 @@ export class LoginComponent implements OnInit {
   private FormUserPass = new FormControl();
 
   //variables dinamicas
-<<<<<<< HEAD
   private rolUser: string = "User";
-=======
-  private rolUser: string = 'Usuario Corriente';
->>>>>>> a6219970dab3fc6ab5a713d0084503d244244c6d
   statusRegistro: any;
   statusLoginIncorrecto: any;
   statusLoginCorrecto: any;
@@ -49,17 +36,8 @@ export class LoginComponent implements OnInit {
   CurrentLocalStorageState: string;
   codeErrorGUI: string;
 
-<<<<<<< HEAD
-  constructor(private BuilderRegistroForm: FormBuilder, private BuilderLoginForm: FormBuilder, private WebServiceUser: LoginService,private enrutamiento:Router) {
+  constructor(private BuilderRegistroForm: FormBuilder, private BuilderLoginForm: FormBuilder, private WebServiceUser: LoginService, private enrutamiento: Router) {
 
-=======
-  constructor(
-    private BuilderRegistroForm: FormBuilder,
-    private BuilderLoginForm: FormBuilder,
-    private WebServiceUser: LoginService,
-    private navegacionRouter:Router
-  ) {
->>>>>>> a6219970dab3fc6ab5a713d0084503d244244c6d
     this.FormRegistroUser = this.BuilderRegistroForm.group({
       FormName: ['', Validators.required],
       FormLastName: ['', Validators.required],
@@ -81,7 +59,6 @@ export class LoginComponent implements OnInit {
       Phone: this.FormRegistroUser.get('FormPhone').value,
       Email: this.FormRegistroUser.get('FormEmail').value,
       Pass: this.FormRegistroUser.get('FormPass').value,
-<<<<<<< HEAD
       Role: this.rolUser
     }
 
@@ -103,20 +80,20 @@ export class LoginComponent implements OnInit {
 
       Email: this.FormLogin.get('FormUserEmail').value,
       Pass: this.FormLogin.get('FormUserPass').value,
-      LastName:"",
-      Name:"",
-      Phone:"",
-      Role:"Admin",
-      id:""
-      
+      LastName: "",
+      Name: "",
+      Phone: "",
+      Role: "Admin",
+      id: ""
+
     }
 
     this.WebServiceUser.PostUserAutentication(credentials).subscribe(
-      (result:any) => {
-        alert("Solicitud: "+ JSON.stringify(result))
+      (result: any) => {
+        alert("Solicitud: " + JSON.stringify(result))
       },
-      (error:HttpErrorResponse) =>{
-        
+      (error: HttpErrorResponse) => {
+
         alert(JSON.stringify(error.error));
 
         this.statusLoginIncorrecto = true;
@@ -126,37 +103,35 @@ export class LoginComponent implements OnInit {
       }
     )
 
-   
+
 
   }
 
   ngOnInit(): void {
-=======
-      Rol: this.rolUser,
+    Rol: this.rolUser,
     };
 
     this.WebServiceUser.PostUser(registro).subscribe(
-      (result) => {},
+      (result) => { },
       (RegistroCompleto: any) => {
         this.statusRegistro = true;
       }
     );
   }
 
-  ServiceAutentication() {
-    const registro: AuthModel = {
-      Email: this.FormLogin.get('FormUserEmail').value,
-      Pass: this.FormLogin.get('FormUserPass').value,
-    };
+ServiceAutentication() {
+  const registro: AuthModel = {
+    Email: this.FormLogin.get('FormUserEmail').value,
+    Pass: this.FormLogin.get('FormUserPass').value,
+  };
 
-    this.WebServiceUser.PostAuth(registro).subscribe(
-      (start) => {
-        alert(JSON.stringify(start));
-      },
-      
-    );
->>>>>>> a6219970dab3fc6ab5a713d0084503d244244c6d
-  }
+  this.WebServiceUser.PostAuth(registro).subscribe(
+    (start) => {
+      alert(JSON.stringify(start));
+    },
 
-  ngOnInit() {}
+  );
+}
+
+ngOnInit() { }
 }
