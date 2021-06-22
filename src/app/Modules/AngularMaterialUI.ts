c26262945c1ef6456056eb75c9a8bf1b5d2ce0a6
cgonzalezh333 <claudiogh33@gmail.com>
2021-06-21 22:57:11 -0600

    angular material

---
+++ src/app/Modules/AngularMaterialUI.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatCardModule } from "@angular/material/card";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSortModule } from "@angular/material/sort";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatSelectModule } from "@angular/material/select";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from "@angular/material/icon";
+
+const MaterialComponents = [
+MatButtonModule,
+MatInputModule,
+MatFormFieldModule,
+MatCardModule,
+MatTableModule,
+MatPaginatorModule,
+MatSortModule,
+MatSlideToggleModule,
+MatSelectModule,
+MatDatepickerModule,
+MatNativeDateModule,
+MatIconModule
+];
+ 
+@NgModule({
+ imports:  [MaterialComponents],
+ exports: [MaterialComponents]
+})
+export class AngularUI { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
+import { AngularUI } from 'src/app/Modules/AngularMaterialUI';
 //componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from 'src/app/Components/login/login.component';
@@ -15,6 +16,7 @@ import { UsersDatabaseComponent } from './Components/users-database/users-databa
 import { EmailDashboardComponent } from './Components/email-dashboard/email-dashboard.component';
 import { ContactListComponent } from './Components/contact-list/contact-list.component';
 import { InventoryComponent } from './Components/inventory/inventory.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -35,7 +37,9 @@ import { InventoryComponent } from './Components/inventory/inventory.component';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AngularUI
   ],
   providers: [],
   bootstrap: [AppComponent]
